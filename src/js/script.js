e6893a4f2fd1672dae8e2654d304b159884a3fbd
Kramarenko Maxim <kramarenko-90@inbox.ru>
2023-06-20 18:05:32 +0300

    modal

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,45 @@
 document.addEventListener('DOMContentLoaded', () => {
+
+    // modal
+
+    function openModal(modalSelector) {
+        const modal = document.querySelector(modalSelector);
+        modal.classList.add('fade');
+        modal.style.display = 'block';
+    }
+
+    function closeModal (modalSelector) {
+        const modal = document.querySelector(modalSelector);
+        modal.classList.remove('fade');
+        modal.style.display = 'none';
+    }
+
+    function modal (modalSelector, triggerSelector, closeSelector) {
+        const modal = document.querySelector(modalSelector),
+              modalTrigger = document.querySelectorAll(triggerSelector),
+              modalClose = document.querySelector(closeSelector);
+
+        modalTrigger.forEach(btn => {
+            btn.addEventListener('click', () => openModal(modalSelector));
+        });
+
+        modal.addEventListener('click', (e) => {
+            if(e.target === modal || e.target.getAttribute(closeSelector) == '') {
+                closeModal(modalSelector);
+            }
+        });
+
+        document.addEventListener('keydown', (e) => {
+            if(e.code === 'Escape') {
+                closeModal(modalSelector);
+            }
+        });
+    }
+
+    modal('.modal', '[data-modal]', 'data-close');
+
+    // accordion
+    
     const accordions = document.querySelectorAll('.accordion');
     const arrow = document.getElementById("arrow-svg");
 
